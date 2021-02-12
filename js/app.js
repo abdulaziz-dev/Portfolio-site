f6590cbdb3f413c4fa80aa10a0e648d1a181c11e
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2021-02-12 10:45:16 +0500

    ToTop button added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+const toTop = document.querySelector('.to-top')
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 window.addEventListener("scroll", function () {
 	var navScroll = document.getElementById('nav');
-	navScroll.classList.toggle("fixnav", window.scrollY > 150);
+	navScroll.classList.toggle("fixnav", window.scrollY > 200);
 })
-
 function navBack(){
 	var nav = document.getElementById('nav');
 	nav.classList.toggle("change");
